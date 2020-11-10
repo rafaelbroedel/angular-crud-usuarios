7cc559e6eccb2280809866d30a9559cdb15a0865
Rafael Broédel Pedroni <rafael_broedel@hotmail.com>
2020-11-10 17:25:37 -0300

    Diagramar tela de login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
